cd36d7acd8ae8cdc8955596cb65f3a995a831b1d
BOUSFIHA Rania <12300494@q10411.iutv.univ-paris13.fr>
2025-05-21 11:23:50 +0200

    modif

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,6 +2,7 @@ package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,12 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest {
 
     @Test
-    void creerVoiture() {
-        Voiture voiture = new Voiture("Toyota", 15000);
-        voiture.setId(1);
+    void creerVoiture(){
+        Voiture tonobil = new Voiture("ferari",500);
+        tonobil.setId(2);
+        assertEquals(tonobil.getId(), 2);
+        assertEquals(tonobil.getMarque(), "ferari");
+        assertEquals(tonobil.getPrix(), 500 );
+        tonobil.setPrix(600);
+        tonobil.setMarque("zz");
+        assertEquals(tonobil.getMarque(), "zz");
+        assertEquals(tonobil.getPrix(), 600 );
 
-        assertEquals("Toyota", voiture.getMarque());
-        assertEquals(15000, voiture.getPrix());
-        assertEquals(1, voiture.getId());
     }
+
 }
