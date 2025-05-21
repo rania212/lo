99a0039038a511a11f001ad08be245452ef28d38
BOUSFIHA Rania <12300494@q10411.iutv.univ-paris13.fr>
2025-05-21 11:02:40 +0200

    newcarservice

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.mockito.Mockito.*;
-
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class StatistiqueTests {
@@ -14,4 +14,19 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    public void testPrixMoyenAvecMock() {
+        // Création d’un faux Echantillon à retourner
+        Echantillon fauxEchantillon = new Echantillon(3, 20000);
+
+        // Simulation du comportement de la méthode prixMoyen()
+        when(statistiqueImpl.prixMoyen()).thenReturn(fauxEchantillon);
+
+        // Appel de la méthode mockée
+        Echantillon resultat = statistiqueImpl.prixMoyen();
+
+        // Vérifications
+        assertEquals(3, resultat.getNombreDeVoitures());
+        assertEquals(20000, resultat.getPrixMoyen());
+    }
 }
